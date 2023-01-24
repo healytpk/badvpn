2e5343ce61f20c1125cee4e040dedf45b51acb81
healytpk <thomaspkhealy@yahoo.com>
2023-01-24 08:18:54 +0000

    Update build scripts for latest version of badvpn

--- tun2socks/build.sh
+++ tun2socks/build.sh
@@ -1,20 +1,12 @@
-#!/bin/sh
 gcc -o libtun2socks.so -shared -fPIC -Wl,--no-undefined \
--rdynamic \
 CMakeFiles/badvpn-tun2socks.dir/tun2socks.c.o \
 CMakeFiles/badvpn-tun2socks.dir/SocksUdpGwClient.c.o \
 -L/usr/local/lib -Wl,-rpath,/usr/local/lib: \
--lrt \
-../system/libbadvpn-system.a \
-../flow/libbadvpn-flow.a \
-../tuntap/libbadvpn-tuntap.a \
-../lwip/libbadvpn-lwip.a \
-../socksclient/libbadvpn-socksclient.a \
-../udpgw_client/libbadvpn-udpgw_client.a \
-../flowextra/libbadvpn-flowextra.a \
-../system/libbadvpn-system.a \
-../flow/libbadvpn-flow.a \
-../base/libbadvpn-base.a \
-../socks_udp_client/libbadvpn-socks_udp_client.a \
-../system/libbadvpn-system.a \
--lrt
+-lrt -lpthread \
+../system/libbadvpn-system.a ../flow/libbadvpn-flow.a ../tuntap/libbadvpn-tuntap.a \
+../lwip/libbadvpn-lwip.a ../socksclient/libbadvpn-socksclient.a \
+../udpgw_client/libbadvpn-udpgw_client.a ../socks_udp_client/libbadvpn-socks_udp_client.a \
+../socksclient/libbadvpn-socksclient.a ../flowextra/libbadvpn-flowextra.a \
+../system/libbadvpn-system.a ../flow/libbadvpn-flow.a ../base/libbadvpn-base.a \
+-lrt -lpthread \
+-rdynamic
--- tun2socks/build_original.sh
+++ tun2socks/build_original.sh
@@ -1,17 +1,12 @@
-#!/bin/sh
-gcc -rdynamic \
+gcc -o badvpn-tun2socks \
 CMakeFiles/badvpn-tun2socks.dir/tun2socks.c.o \
 CMakeFiles/badvpn-tun2socks.dir/SocksUdpGwClient.c.o \
--o badvpn-tun2socks \
 -L/usr/local/lib -Wl,-rpath,/usr/local/lib: \
--lrt \
-../system/libsystem.a \
-../flow/libflow.a \
-../tuntap/libtuntap.a \
-../lwip/liblwip.a \
-../socksclient/libsocksclient.a \
-../udpgw_client/libudpgw_client.a \
-../flowextra/libflowextra.a \
-../system/libsystem.a \
-../flow/libflow.a \
-../base/libbase.a -lrt
+-lrt -lpthread \
+../system/libbadvpn-system.a ../flow/libbadvpn-flow.a ../tuntap/libbadvpn-tuntap.a \
+../lwip/libbadvpn-lwip.a ../socksclient/libbadvpn-socksclient.a \
+../udpgw_client/libbadvpn-udpgw_client.a ../socks_udp_client/libbadvpn-socks_udp_client.a \
+../socksclient/libbadvpn-socksclient.a ../flowextra/libbadvpn-flowextra.a \
+../system/libbadvpn-system.a ../flow/libbadvpn-flow.a ../base/libbadvpn-base.a \
+-lrt -lpthread \
+-rdynamic
